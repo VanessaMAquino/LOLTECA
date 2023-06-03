ef847e0bdfad7a683f5f7bc7166f071db6c86c81
VanessaMAquino <vanessa_mirele@hotmail.com>
2023-06-02 21:38:06 -0300

    Quiz finalizado só falta arrumar o css

--- site/public/js/quiz.js
+++ site/public/js/quiz.js
@@ -1,4 +1,4 @@
-b_usuario.innerHTML = sessionStorage.NOME_USUARIO;
+a_usuario.innerHTML = sessionStorage.NOME_USUARIO;
 var idUser = sessionStorage.ID_USUARIO
 
 var contador = 0
@@ -17,115 +17,115 @@ inicioQuiz.addEventListener("click", () => {
 
 function atualizar() {
     const Perguntas = [{
-        pergunta: "Agora vocês podem me admirar",
-        resposta: [{ text: "Ahri", isCorrect: false },
-        { text: "Renata Glasc", isCorrect: false },
-        { text: "Qiyana", isCorrect: true },
-        { text: "Evelynn", isCorrect: false }
+        pergunta: "Darius",
+        resposta: [{ text: "Mid", isCorrect: false },
+        { text: "Jungle", isCorrect: false },
+        { text: "Top", isCorrect: true },
+        { text: "Bot", isCorrect: false }
         ]
 
     },
     {
-        pergunta: "Precisão é a diferença entre um açougueiro e um cirurgião",
-        resposta: [{ text: "Camille", isCorrect: true },
-        { text: "Shen", isCorrect: false },
-        { text: "Kennen", isCorrect: false },
-        { text: "Olaf", isCorrect: false }
+        pergunta: "Malphite",
+        resposta: [{ text: "Top", isCorrect: true },
+        { text: "Bot", isCorrect: false },
+        { text: "Jungle", isCorrect: false },
+        { text: "Mid", isCorrect: false }
         ]
 
     },
     {
-        pergunta: "Não confie em nada além da sua força",
-        resposta: [{ text: "Darius", isCorrect: false },
-        { text: "Trundle", isCorrect: false },
-        { text: "Ornn", isCorrect: false },
-        { text: "Sejuani", isCorrect: true }
+        pergunta: "Rengar",
+        resposta: [{ text: "Top", isCorrect: false },
+        { text: "Mid", isCorrect: false },
+        { text: "Bot", isCorrect: false },
+        { text: "Jungle", isCorrect: true }
         ]
 
     },
     {
-        pergunta: "A rosa negra irá desabrochar novamente",
-        resposta: [{ text: "Zyra", isCorrect: false },
-        { text: "Illaoi", isCorrect: false },
-        { text: "Leblanc", isCorrect: true },
-        { text: "Gwen", isCorrect: false }
+        pergunta: "Lux",
+        resposta: [{ text: "Sup", isCorrect: false },
+        { text: "Top", isCorrect: false },
+        { text: "Mid", isCorrect: true },
+        { text: "Bot", isCorrect: false }
         ]
 
     },
     {
-        pergunta: "Enquanto eu viver ninguém há de sofrer",
-        resposta: [{ text: "Seraphine", isCorrect: false },
-        { text: "Morgana", isCorrect: false },
-        { text: "Janna", isCorrect: false },
-        { text: "Soraka", isCorrect: true }
+        pergunta: "Jinx",
+        resposta: [{ text: "Mid", isCorrect: false },
+        { text: "Top", isCorrect: false },
+        { text: "Jungle", isCorrect: false },
+        { text: "Bot", isCorrect: true }
         ]
 
     },
     {
-        pergunta: "Eu sou o terror de todos que se opõem à mim",
-        resposta: [{ text: "Darius", isCorrect: false },
-        { text: "Nocturne", isCorrect: false },
-        { text: "Shyvana", isCorrect: true },
-        { text: "Mordekaiser", isCorrect: false }
+        pergunta: "Lucian",
+        resposta: [{ text: "Top", isCorrect: false },
+        { text: "Jungle", isCorrect: false },
+        { text: "Bot", isCorrect: true },
+        { text: "Mid", isCorrect: false }
         ]
 
     },
     {
-        pergunta: "Eu serei livre",
-        resposta: [{ text: "Nidalee", isCorrect: false },
-        { text: "Ryze", isCorrect: false },
-        { text: "Xerath", isCorrect: true },
-        { text: "Lee sin", isCorrect: false }
+        pergunta: "Leona",
+        resposta: [{ text: "Top", isCorrect: false },
+        { text: "Jungle", isCorrect: false },
+        { text: "Bot", isCorrect: true },
+        { text: "Top", isCorrect: false }
         ]
 
     },
     {
-        pergunta: "Alegria! Incessante e eterna",
-        resposta: [{ text: "Nilah", isCorrect: true },
-        { text: "Senna", isCorrect: false },
-        { text: "Seraphine", isCorrect: false },
-        { text: "Akshan", isCorrect: false }
+        pergunta: "Zed",
+        resposta: [{ text: "Mid", isCorrect: true },
+        { text: "Top", isCorrect: false },
+        { text: "Sup", isCorrect: false },
+        { text: "Bot", isCorrect: false }
         ]
 
     },
     {
-        pergunta: "Não tem pra ninguem!",
-        resposta: [{ text: "Trundle", isCorrect: false },
-        { text: "Draven", isCorrect: false },
-        { text: "Darius", isCorrect: false },
-        { text: "Sett", isCorrect: true}
+        pergunta: "Morgana",
+        resposta: [{ text: "Top", isCorrect: false },
+        { text: "Mid", isCorrect: false },
+        { text: "Jungle", isCorrect: false },
+        { text: "Bot", isCorrect: true}
         ]
 
     },
     {
-        pergunta: "Vão tremer diante da minha perfeição",
-        resposta: [{ text: "Ahri", isCorrect: false },
-        { text: "Leona", isCorrect: false },
-        { text: "Diana", isCorrect: false },
-        { text: "Kayle", isCorrect: true }
+        pergunta: "Elise",
+        resposta: [{ text: "Top", isCorrect: false },
+        { text: "Bot", isCorrect: false },
+        { text: "Mid", isCorrect: false },
+        { text: "Jungle", isCorrect: true }
         ]
 
     }
     ]
 
     const frase = document.querySelector('#pergunta')
-    const alternativa1 = document.querySelector('#op1')
-    const alternativa2 = document.querySelector('#op2')
-    const alternativa3 = document.querySelector('#op3')
-    const alternativa4 = document.querySelector('#op4')
+    const campeao1 = document.querySelector('#op1')
+    const campeao2 = document.querySelector('#op2')
+    const campeao3 = document.querySelector('#op3')
+    const campeao4 = document.querySelector('#op4')
 
     frase.innerText = Perguntas[contador].pergunta
-    alternativa1.innerText = Perguntas[contador].resposta[0].text
-    alternativa2.innerText = Perguntas[contador].resposta[1].text
-    alternativa3.innerText = Perguntas[contador].resposta[2].text
-    alternativa4.innerText = Perguntas[contador].resposta[3].text
+    campeao1.innerText = Perguntas[contador].resposta[0].text
+    campeao2.innerText = Perguntas[contador].resposta[1].text
+    campeao3.innerText = Perguntas[contador].resposta[2].text
+    campeao4.innerText = Perguntas[contador].resposta[3].text
 
-    alternativa1.value = Perguntas[contador].resposta[0].isCorrect
-    alternativa2.value = Perguntas[contador].resposta[1].isCorrect
-    alternativa3.value = Perguntas[contador].resposta[2].isCorrect
-    alternativa4.value = Perguntas[contador].resposta[3].isCorrect
+    campeao1.value = Perguntas[contador].resposta[0].isCorrect
+    campeao2.value = Perguntas[contador].resposta[1].isCorrect
+    campeao3.value = Perguntas[contador].resposta[2].isCorrect
+    campeao4.value = Perguntas[contador].resposta[3].isCorrect
 
-    alternativa1.addEventListener("click", () => {
+    campeao1.addEventListener("click", () => {
         op1.style.color = '#000000'
         op2.style.color = '#FFFFFF'
         op3.style.color = '#FFFFFF'
@@ -134,10 +134,10 @@ function atualizar() {
         op2.style.backgroundColor = "#000000";
         op3.style.backgroundColor = "#000000";
         op4.style.backgroundColor = "#000000";
-        resposta = alternativa1.value;
+        resposta = campeao1.value;
     })
 
-    alternativa2.addEventListener("click", () => {
+    campeao2.addEventListener("click", () => {
         op1.style.color = '#FFFFFF'
         op2.style.color = '#000000'
         op3.style.color = '#FFFFFF'
@@ -146,10 +146,10 @@ function atualizar() {
         op2.style.backgroundColor = "#FFFFFF";
         op3.style.backgroundColor = "#000000";
         op4.style.backgroundColor = "#000000";
-        resposta = alternativa2.value;
+        resposta = campeao2.value;
     })
 
-    alternativa3.addEventListener("click", () => {
+    campeao3.addEventListener("click", () => {
         op1.style.color = '#FFFFFF'
         op2.style.color = '#FFFFFF'
         op3.style.color = '#000000'
@@ -158,10 +158,10 @@ function atualizar() {
         op2.style.backgroundColor = "#000000";
         op3.style.backgroundColor = "#FFFFFF";
         op4.style.backgroundColor = "#000000";
-        resposta = alternativa3.value;
+        resposta = campeao3.value;
     })
 
-    alternativa4.addEventListener("click", () => {
+    campeao4.addEventListener("click", () => {
         op1.style.backgroundColor = "#000000";
         op2.style.backgroundColor = "#000000";
         op3.style.backgroundColor = "#000000";
@@ -170,7 +170,7 @@ function atualizar() {
         op3.style.color = '#FFFFFF'
         op4.style.color = '#000000'
         op4.style.backgroundColor = "#FFFFFF";
-        resposta = alternativa4.value;
+        resposta = campeao4.value;
     })
 }
 
@@ -199,25 +199,25 @@ function finalizarQuiz() {
     conteudo.style.display = 'none'
     nota.style.display = 'flex'
     if (pontos <= 3) {
-        imagem__nota.src = 'img/AmumuIcon_16.webp'
+        imagemNota.src = 'img/soraka.gif'
         nota.style.backgroundColor = '#000000'
         nota.style.color = '#FFFFFF'
-        botao__finalizar.style.backgroundColor = '#FFFFFF'
-        botao__finalizar.style.color = '#000000'
+        botaoFinalizar.style.backgroundColor = '#FFFFFF'
+        botaoFinalizar.style.color = '#000000'
     }
     else if (pontos <= 6) {
-        imagem__nota.src = 'img/gif__vi.gif'
+        imagemNota.src = 'img/amumu.gif'
         nota.style.backgroundColor = '#000000'
         nota.style.color = '#FFFFFF'
-        botao__finalizar.style.backgroundColor = '#FFFFFF'
-        botao__finalizar.style.color = '#000000'
+        botaoFinalizar.style.backgroundColor = '#FFFFFF'
+        botaoFinalizar.style.color = '#000000'
     }
     else {
-        imagem__nota.src = 'img/Zilean_Gif.webp'
+        imagemNota.src = 'img/Zilean_Gif.webp'
         nota.style.backgroundColor = '#000000'
         nota.style.color = '#FFFFFF'
-        botao__finalizar.style.backgroundColor = '#FFFFFF'
-        botao__finalizar.style.color = '#000000'
+        botaoFinalizar.style.backgroundColor = '#FFFFFF'
+        botaoFinalizar.style.color = '#000000'
     }
     notaFinal.innerHTML = `Você acertou: ${pontos} de 10!`
 }
@@ -233,5 +233,6 @@ function finalizar() {
             idServer: idUser
         })
     });
-    window.location = 'classificacao.html'
+    window.location = 'quiz.html'
 }
+
